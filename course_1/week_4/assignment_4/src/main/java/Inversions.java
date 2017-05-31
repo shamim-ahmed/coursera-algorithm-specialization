b577fc069600ada2911ebf592bd7e301d09be27b
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-05-31 23:02:32 +1000

    added an implementation of merge sort

---
+++ course_1/week_4/assignment_4/src/main/java/Inversions.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class Inversions {
+  private static final int[] EMPTY_ARRAY = new int[0];
+
+  private static int[] mergeSort(int[] a, int left, int right) {
+    if (left > right) {
+      return EMPTY_ARRAY;
+    }
+
+    if (left == right) {
+      return new int[] {a[left]};
+    }
+
+    int mid = (left + right) / 2;
+    int[] p = mergeSort(a, left, mid);
+    int[] q = mergeSort(a, mid + 1, right);
+    int[] result = merge(p, q);
+
+    return result;
+  }
+
+  private static int[] merge(int[] a, int[] b) {
+    int[] result = new int[a.length + b.length];
+    int i = 0, j = 0, k = 0;
+
+    while (i < a.length && j < b.length) {
+      if (a[i] <= b[j]) {
+        result[k] = a[i];
+        i++;
+      } else {
+        result[k] = b[j];
+        j++;
+      }
+
+      k++;
+    }
+
+    while (i < a.length) {
+      result[k] = a[i];
+      i++;
+      k++;
+    }
+
+    while (j < b.length) {
+      result[k] = b[j];
+      j++;
+      k++;
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int[] a = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      a[i] = scanner.nextInt();
+    }
+
+    int[] result = mergeSort(a, 0, a.length - 1);
+    System.out.println(Arrays.toString(result));
+    scanner.close();
+  }
+}
