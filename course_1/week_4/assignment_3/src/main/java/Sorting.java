8ec9551d98f4183cb8bbff5ba1fbae3030d5cea8
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-06-03 20:26:41 +1000

    added tentative solution to assignment 3

--- course_1/week_4/assignment_3/src/main/java/Sorting.java
+++ course_1/week_4/assignment_3/src/main/java/Sorting.java
@@ -2,88 +2,122 @@ import java.io.*;
 import java.util.*;
 
 public class Sorting {
-    private static Random random = new Random();
-
-    private static int[] partition3(int[] a, int l, int r) {
-      //write your code here
-
-
-      int m1 = l;
-      int m2 = r;
-      int[] m = {m1, m2};
-      return m;
+  private static Random random = new Random();
+  
+  private static int[] partition3(int[] a, int left, int right) {
+    int pivot = a[right];
+    int i = left - 1;
+    int j = right;
+    int x = left - 1;
+    int y = right;
+    
+    while (true) {
+      do {
+        i++;
+      } while (a[i] < pivot);
+      
+      do {
+        j--;
+      } while (a[j] > pivot && j > left);
+      
+      if (i >= j) {
+        break;
+      }
+      
+      swap(a, i, j);
+      
+      if (a[i] == pivot) {
+        x++;
+        swap(a, x, i);
+      }
+      
+      if (a[j] == pivot) {
+        y--;
+        swap(a, y, j);
+      }
+    }
+    
+    swap(a, i, right);
+    
+    int p = i - 1;
+    
+    for (int k = left; k <= x; k++, p--) {
+      swap(a, k, p);
+    }
+    
+    int q = i + 1;
+    
+    for (int k = right - 1; k >= y; k--, q++) {
+      swap(a, k, q);
+    } 
+    
+    int[] result = new int[2];
+    result[0] = p;
+    result[1] = q;
+    
+    return result;
+  }
+  
+  private static void randomizedQuickSort(int[] a, int l, int r) {
+    if (l >= r) {
+      return;
     }
 
-    private static int partition2(int[] a, int l, int r) {
-        int x = a[l];
-        int j = l;
-        for (int i = l + 1; i <= r; i++) {
-            if (a[i] <= x) {
-                j++;
-                int t = a[i];
-                a[i] = a[j];
-                a[j] = t;
-            }
-        }
-        int t = a[l];
-        a[l] = a[j];
-        a[j] = t;
-        return j;
+    int k = random.nextInt(r - l + 1) + l;
+    int t = a[l];
+    a[l] = a[k];
+    a[k] = t;
+
+    // use partition3
+    int[] m = partition3(a, l, r);
+    randomizedQuickSort(a, l, m[0]);
+    randomizedQuickSort(a, m[1], r);
+  }
+  
+  private static void swap(int[] a, int i, int j) {
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+  }
+
+  public static void main(String[] args) {
+    FastScanner scanner = new FastScanner(System.in);
+    int n = scanner.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = scanner.nextInt();
+    }
+    randomizedQuickSort(a, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+      System.out.print(a[i] + " ");
+    }
+  }
+
+  static class FastScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    FastScanner(InputStream stream) {
+      try {
+        br = new BufferedReader(new InputStreamReader(stream));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
 
-    private static void randomizedQuickSort(int[] a, int l, int r) {
-        if (l >= r) {
-            return;
+    String next() {
+      while (st == null || !st.hasMoreTokens()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
         }
-        int k = random.nextInt(r - l + 1) + l;
-        int t = a[l];
-        a[l] = a[k];
-        a[k] = t;
-        //use partition3
-        int m = partition2(a, l, r);
-        randomizedQuickSort(a, l, m - 1);
-        randomizedQuickSort(a, m + 1, r);
+      }
+      return st.nextToken();
     }
 
-    public static void main(String[] args) {
-        FastScanner scanner = new FastScanner(System.in);
-        int n = scanner.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-        }
-        randomizedQuickSort(a, 0, n - 1);
-        for (int i = 0; i < n; i++) {
-            System.out.print(a[i] + " ");
-        }
+    int nextInt() {
+      return Integer.parseInt(next());
     }
-
-    static class FastScanner {
-        BufferedReader br;
-        StringTokenizer st;
-
-        FastScanner(InputStream stream) {
-            try {
-                br = new BufferedReader(new InputStreamReader(stream));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreTokens()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-    }
+  }
 }
-
