da98693515e56886f0bd430f826035afbe50cb86
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-06-04 21:33:47 +1000

    used access modifiers consistently

--- course_2/week_1/assignment_2/src/main/java/edu/buet/cse/demo/PreOrderTraversal.java
+++ course_2/week_1/assignment_2/src/main/java/edu/buet/cse/demo/PreOrderTraversal.java
@@ -10,44 +10,44 @@ import java.util.StringTokenizer;
 
 public class PreOrderTraversal {
   private static final int INVALID_INDEX = -1;
-  
-  static class FastScanner {
-    StringTokenizer tok = new StringTokenizer("");
-    BufferedReader in;
 
-    FastScanner() {
+  private static class FastScanner {
+    private StringTokenizer tok = new StringTokenizer("");
+    private BufferedReader in;
+
+    public FastScanner() {
       in = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    String next() throws IOException {
+    public String next() throws IOException {
       while (!tok.hasMoreElements())
         tok = new StringTokenizer(in.readLine());
       return tok.nextToken();
     }
 
-    int nextInt() throws IOException {
+    public int nextInt() throws IOException {
       return Integer.parseInt(next());
     }
   }
-  
-  static class Node {
+
+  private static class Node {
     private final int id;
     private Node parent;
     private final List<Node> childNodes;
-    
+
     public Node(int id) {
       this.id = id;
       childNodes = new ArrayList<>();
     }
-    
+
     public int getId() {
       return id;
     }
-    
+
     public Node getParent() {
       return parent;
     }
-    
+
     public void setParent(Node parent) {
       this.parent = parent;
     }
@@ -55,30 +55,29 @@ public class PreOrderTraversal {
     public List<Node> getChildNodes() {
       return childNodes;
     }
-    
+
     public void addChild(Node child) {
       childNodes.add(child);
     }
   }
 
-  static class TreeHeight {
-    int n;
-    Node nodeArray[];
-    Node root;
+  private static class TreeHeight {
+    private Node nodeArray[];
+    private Node root;
 
-    void read() throws IOException {
+    public void read() throws IOException {
       FastScanner in = new FastScanner();
-      n = in.nextInt();
+      final int n = in.nextInt();
       nodeArray = new Node[n];
-      
+
       for (int i = 0; i < n; i++) {
         nodeArray[i] = new Node(i);
       }
-      
+
       for (int i = 0; i < n; i++) {
         Node node = nodeArray[i];
         int parentIndex = in.nextInt();
-        
+
         if (parentIndex == INVALID_INDEX) {
           node.setParent(null);
           root = node;
@@ -90,28 +89,28 @@ public class PreOrderTraversal {
       }
     }
 
-    void traversePreOrder() {
+    public void traversePreOrder() {
       Stack<Node> nodeStack = new Stack<>();
       nodeStack.push(root);
-      
+
       while (!nodeStack.isEmpty()) {
         Node node = nodeStack.pop();
         System.out.printf("%3d", node.getId());
-        
+
         List<Node> childNodes = node.getChildNodes();
         int n = childNodes.size();
-        
+
         for (int i = n - 1; i >= 0; i--) {
           Node child = childNodes.get(i);
           nodeStack.push(child);
         }
       }
-      
+
       System.out.println();
     }
   }
 
-  static public void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
     new Thread(null, new Runnable() {
       public void run() {
         try {
