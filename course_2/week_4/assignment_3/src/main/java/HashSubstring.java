810fe4338aa897dcfd19ddc51b697831af736b7b
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-06-25 01:19:28 +1000

    added a non-optimized implementation of Rabin-Karp algorithm

--- course_2/week_4/assignment_3/src/main/java/HashSubstring.java
+++ course_2/week_4/assignment_3/src/main/java/HashSubstring.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class HashSubstring {
+  private static final int PRIME = 1000000007;
+  private static final int MULTIPLIER = 263;
 
   private static FastScanner in;
   private static PrintWriter out;
@@ -12,52 +14,108 @@ public class HashSubstring {
   public static void main(String[] args) throws IOException {
     in = new FastScanner();
     out = new PrintWriter(new BufferedOutputStream(System.out));
-    printOccurrences(getOccurrences(readInput()));
+
+    Data input = readInput();
+    List<Integer> occurrenceList = getOccurrences(input);
+    printOccurrences(occurrenceList);
+
     out.close();
   }
 
   private static Data readInput() throws IOException {
     String pattern = in.next();
     String text = in.next();
+
     return new Data(pattern, text);
   }
 
-  private static void printOccurrences(List<Integer> ans) throws IOException {
-    for (Integer cur : ans) {
-      out.print(cur);
+  private static void printOccurrences(List<Integer> occurrenceList) throws IOException {
+    for (Integer idx : occurrenceList) {
+      out.print(idx);
       out.print(" ");
     }
   }
 
   private static List<Integer> getOccurrences(Data input) {
-    String s = input.pattern, t = input.text;
-    int m = s.length(), n = t.length();
-    List<Integer> occurrences = new ArrayList<Integer>();
-    for (int i = 0; i + m <= n; ++i) {
-      boolean equal = true;
-      for (int j = 0; j < m; ++j) {
-        if (s.charAt(j) != t.charAt(i + j)) {
-          equal = false;
-          break;
-        }
+    String pattern = input.getPattern();
+    char[] patternChars = pattern.toCharArray();
+    String text = input.getText();
+    char[] textChars = text.toCharArray();
+    int patternLength = pattern.length();
+    int textLength = text.length();
+    List<Integer> occurrenceList = new ArrayList<Integer>();
+
+    long pHash = getHashValueFor(patternChars, 0, patternLength);
+    long[] hashValues = computeHashes(textChars, textLength, patternLength);
+
+    for (int i = 0; i <= textLength - patternLength; i++) {
+      if (hashValues[i] != pHash) {
+        continue;
       }
-      if (equal)
-        occurrences.add(i);
+
+      if (isEqual(textChars, i, patternChars)) {
+        occurrenceList.add(i);
+      }
     }
-    return occurrences;
+
+    return occurrenceList;
   }
 
-  static class Data {
-    String pattern;
-    String text;
+  private static long[] computeHashes(char[] textChars, int textLength, int patternLength) {
+    final int n = textLength - patternLength + 1;
+    long[] hashValues = new long[n];
+
+    for (int i = 0; i < n; i++) {
+      hashValues[i] = getHashValueFor(textChars, i, patternLength);
+    }
+    
+    return hashValues;
+  }
+
+  private static long getHashValueFor(char[] charArray, int i, int patternLength) {
+    long hc = 0;
+    long x = 1L;
+
+    for (int j = i; j < i + patternLength; j++) {
+      hc = (hc + ((long) charArray[j]) * x) % PRIME;
+      x = (x * MULTIPLIER) % PRIME;
+    }
+
+    return hc;
+  }
+
+  private static boolean isEqual(char[] textChars, int startIndex, char[] patternChars) {
+    boolean result = true;
+
+    for (int i = 0; i < patternChars.length; i++) {
+      if (textChars[startIndex + i] != patternChars[i]) {
+        result = false;
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  private static class Data {
+    private final String pattern;
+    private final String text;
 
     public Data(String pattern, String text) {
       this.pattern = pattern;
       this.text = text;
     }
+
+    public String getPattern() {
+      return pattern;
+    }
+
+    public String getText() {
+      return text;
+    }
   }
 
-  static class FastScanner {
+  private static class FastScanner {
     private BufferedReader reader;
     private StringTokenizer tokenizer;
 
@@ -70,12 +128,8 @@ public class HashSubstring {
       while (tokenizer == null || !tokenizer.hasMoreTokens()) {
         tokenizer = new StringTokenizer(reader.readLine());
       }
-      return tokenizer.nextToken();
-    }
 
-    public int nextInt() throws IOException {
-      return Integer.parseInt(next());
+      return tokenizer.nextToken();
     }
   }
 }
-
