ecd9135fa452ababad1b62f9e692b3683d6a7276
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-07-10 23:56:00 +1000

    added solution to assignment 2

--- course_2/week_6/assignment_2/src/main/java/is_bst.java
+++ course_2/week_6/assignment_2/src/main/java/is_bst.java
@@ -2,72 +2,107 @@ import java.util.*;
 import java.io.*;
 
 public class is_bst {
-    class FastScanner {
-        StringTokenizer tok = new StringTokenizer("");
-        BufferedReader in;
+  class FastScanner {
+    StringTokenizer tok = new StringTokenizer("");
+    BufferedReader in;
 
-        FastScanner() {
-            in = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        String next() throws IOException {
-            while (!tok.hasMoreElements())
-                tok = new StringTokenizer(in.readLine());
-            return tok.nextToken();
-        }
-        int nextInt() throws IOException {
-            return Integer.parseInt(next());
-        }
+    FastScanner() {
+      in = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public class IsBST {
-        class Node {
-            int key;
-            int left;
-            int right;
-
-            Node(int key, int left, int right) {
-                this.left = left;
-                this.right = right;
-                this.key = key;
-            }
-        }
-
-        int nodes;
-        Node[] tree;
-
-        void read() throws IOException {
-            FastScanner in = new FastScanner();
-            nodes = in.nextInt();
-            tree = new Node[nodes];
-            for (int i = 0; i < nodes; i++) {
-                tree[i] = new Node(in.nextInt(), in.nextInt(), in.nextInt());
-            }
-        }
-
-        boolean isBinarySearchTree() {
-          // Implement correct algorithm here
-          return true;
-        }
+    String next() throws IOException {
+      while (!tok.hasMoreElements())
+        tok = new StringTokenizer(in.readLine());
+      return tok.nextToken();
     }
 
-    static public void main(String[] args) throws IOException {
-        new Thread(null, new Runnable() {
-            public void run() {
-                try {
-                    new is_bst().run();
-                } catch (IOException e) {
-                }
-            }
-        }, "1", 1 << 26).start();
+    int nextInt() throws IOException {
+      return Integer.parseInt(next());
     }
-    public void run() throws IOException {
-        IsBST tree = new IsBST();
-        tree.read();
-        if (tree.solve()) {
-            System.out.println("CORRECT");
-        } else {
-            System.out.println("INCORRECT");
+  }
+
+  public class IsBST {
+    private static final int INVALID_INDEX = -1;
+
+    class Node {
+      int key;
+      int left;
+      int right;
+
+      Node(int key, int left, int right) {
+        this.left = left;
+        this.right = right;
+        this.key = key;
+      }
+    }
+
+    int nodes;
+    Node[] tree;
+
+    void read() throws IOException {
+      FastScanner in = new FastScanner();
+      nodes = in.nextInt();
+      tree = new Node[nodes];
+      for (int i = 0; i < nodes; i++) {
+        tree[i] = new Node(in.nextInt(), in.nextInt(), in.nextInt());
+      }
+    }
+
+    boolean isBinarySearchTree() {
+      int[] keys = inOrder();
+      boolean result = true;
+
+      for (int i = 0; i < keys.length - 1; i++) {
+        // all keys are supposed to be distinct
+        if (keys[i] >= keys[i + 1]) {
+          result = false;
+          break;
         }
+      }
+
+      return result;
     }
+
+    int[] inOrder() {
+      int[] result = new int[nodes];
+      Stack<Integer> stack = new Stack<>();
+      int i = 0;
+      int count = 0;
+
+      while (count < nodes) {
+        while (i != INVALID_INDEX) {
+          stack.push(i);
+          i = tree[i].left;
+        }
+
+        i = stack.pop();
+        result[count] = tree[i].key;
+        count++;
+        i = tree[i].right;
+      }
+
+      return result;
+    }
+  }
+
+  static public void main(String[] args) throws IOException {
+    new Thread(null, new Runnable() {
+      public void run() {
+        try {
+          new is_bst().run();
+        } catch (IOException e) {
+        }
+      }
+    }, "1", 1 << 26).start();
+  }
+
+  public void run() throws IOException {
+    IsBST tree = new IsBST();
+    tree.read();
+    if (tree.isBinarySearchTree()) {
+      System.out.println("CORRECT");
+    } else {
+      System.out.println("INCORRECT");
+    }
+  }
 }
