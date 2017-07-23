f9ccb395f5dcc93a3224aca040b56d9f470129e9
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-07-23 22:16:00 +1000

    - renamed the slides - added solution to assignment 1

--- course_3/week_1/assignment_1/src/main/java/Reachability.java
+++ course_3/week_1/assignment_1/src/main/java/Reachability.java
@@ -3,8 +3,28 @@ import java.util.Scanner;
 
 public class Reachability {
   private static int reach(ArrayList<Integer>[] adj, int x, int y) {
-    // write your code here
-    return 0;
+    boolean[] visited = new boolean[adj.length];
+    explore(x, y, adj, visited);
+
+    return visited[y] ? 1 : 0;
+  }
+
+  private static void explore(int s, int t, ArrayList<Integer>[] adj, boolean[] visited) {
+    if (visited[t]) {
+      return;
+    }
+
+    visited[s] = true;
+
+    if (s == t) {
+      return;
+    }
+
+    for (int v : adj[s]) {
+      if (!visited[v]) {
+        explore(v, t, adj, visited);
+      }
+    }
   }
 
 
@@ -19,15 +39,14 @@ public class Reachability {
     for (int i = 0; i < n; i++) {
       adj[i] = new ArrayList<Integer>();
     }
-    
+
     for (int i = 0; i < m; i++) {
-      int x, y;
-      x = scanner.nextInt();
-      y = scanner.nextInt();
+      int x = scanner.nextInt();
+      int y = scanner.nextInt();
       adj[x - 1].add(y - 1);
       adj[y - 1].add(x - 1);
     }
-    
+
     int x = scanner.nextInt() - 1;
     int y = scanner.nextInt() - 1;
     System.out.println(reach(adj, x, y));
@@ -35,4 +54,3 @@ public class Reachability {
     scanner.close();
   }
 }
-
