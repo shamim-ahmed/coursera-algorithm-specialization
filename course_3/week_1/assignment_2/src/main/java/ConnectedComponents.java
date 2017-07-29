fd0e14d944241841b2820c14b3a5c6bffb6ec302
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-07-29 19:22:38 +1000

    added solution to assignment 2

--- course_3/week_1/assignment_2/src/main/java/ConnectedComponents.java
+++ course_3/week_1/assignment_2/src/main/java/ConnectedComponents.java
@@ -4,8 +4,30 @@ import java.util.Scanner;
 public class ConnectedComponents {
   private static int numberOfComponents(ArrayList<Integer>[] adj) {
     int result = 0;
-    // write your code here
+    boolean[] visited = new boolean[adj.length];
+
+    for (int i = 0; i < adj.length; i++) {
+      if (!visited[i]) {
+        result++;
+        explore(i, adj, visited);
+      }
+    }
+
     return result;
+  }
+
+  private static void explore(int x, ArrayList<Integer>[] adj, boolean[] visited) {
+    if (visited[x]) {
+      return;
+    }
+
+    visited[x] = true;
+
+    for (int y : adj[x]) {
+      if (!visited[y]) {
+        explore(y, adj, visited);
+      }
+    }
   }
 
   public static void main(String[] args) {
@@ -15,15 +37,14 @@ public class ConnectedComponents {
 
     @SuppressWarnings("unchecked")
     ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
-    
+
     for (int i = 0; i < n; i++) {
       adj[i] = new ArrayList<Integer>();
     }
-    
+
     for (int i = 0; i < m; i++) {
-      int x, y;
-      x = scanner.nextInt();
-      y = scanner.nextInt();
+      int x = scanner.nextInt();
+      int y = scanner.nextInt();
       adj[x - 1].add(y - 1);
       adj[y - 1].add(x - 1);
     }
