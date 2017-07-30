a515a7a3a9b0a15997eefad63ee207e0331869de
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-07-30 19:27:47 +1000

    reformatted code

--- course_3/week_2/assignment_2/src/main/java/Toposort.java
+++ course_3/week_2/assignment_2/src/main/java/Toposort.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Toposort {
@@ -18,20 +17,28 @@ public class Toposort {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
     int m = scanner.nextInt();
+    
+    @SuppressWarnings("unchecked")
     ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
+    
     for (int i = 0; i < n; i++) {
       adj[i] = new ArrayList<Integer>();
     }
+    
     for (int i = 0; i < m; i++) {
       int x, y;
       x = scanner.nextInt();
       y = scanner.nextInt();
       adj[x - 1].add(y - 1);
     }
+    
     ArrayList<Integer> order = toposort(adj);
+    
     for (int x : order) {
       System.out.print((x + 1) + " ");
     }
+    
+    scanner.close();
   }
 }
 
