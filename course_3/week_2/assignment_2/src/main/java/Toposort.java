4f52e6f76b7a34d1810874d987aff822216f2b52
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-07-30 19:50:38 +1000

    added solution to assignment 2

--- course_3/week_2/assignment_2/src/main/java/Toposort.java
+++ course_3/week_2/assignment_2/src/main/java/Toposort.java
@@ -1,16 +1,32 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Toposort {
   private static ArrayList<Integer> toposort(ArrayList<Integer>[] adj) {
-    int used[] = new int[adj.length];
-    ArrayList<Integer> order = new ArrayList<Integer>();
-    // write your code here
-    return order;
+    boolean visitFlags[] = new boolean[adj.length];
+    ArrayList<Integer> orderList = new ArrayList<Integer>();
+    
+    for (int i = 0; i < adj.length; i++) {
+      if (!visitFlags[i]) {
+        dfs(i, adj, visitFlags, orderList);
+      }
+    }
+    
+    Collections.reverse(orderList);
+    return orderList;
   }
 
-  private static void dfs(ArrayList<Integer>[] adj, int[] used, ArrayList<Integer> order, int s) {
-    // write your code here
+  private static void dfs(int i, ArrayList<Integer>[] adj, boolean[] visitFlags, ArrayList<Integer> orderList) {
+    visitFlags[i] = true;
+    
+    for (int j : adj[i]) {
+      if (!visitFlags[j]) {
+        dfs(j, adj, visitFlags, orderList);
+      }
+    }
+    
+    orderList.add(i);
   }
 
   public static void main(String[] args) {
@@ -41,4 +57,3 @@ public class Toposort {
     scanner.close();
   }
 }
-
