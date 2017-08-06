9da15619d763a1ab6380643fecac350931664909
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-08-06 12:51:15 +1000

    minor reformatting

--- course_3/week_3/assignment_2/src/main/java/Bipartite.java
+++ course_3/week_3/assignment_2/src/main/java/Bipartite.java
@@ -13,18 +13,22 @@ public class Bipartite {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
     int m = scanner.nextInt();
+
+    @SuppressWarnings("unchecked")
     ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
+
     for (int i = 0; i < n; i++) {
       adj[i] = new ArrayList<Integer>();
     }
+
     for (int i = 0; i < m; i++) {
-      int x, y;
-      x = scanner.nextInt();
-      y = scanner.nextInt();
+      int x = scanner.nextInt();
+      int y = scanner.nextInt();
       adj[x - 1].add(y - 1);
       adj[y - 1].add(x - 1);
     }
+
     System.out.println(bipartite(adj));
+    scanner.close();
   }
 }
-
