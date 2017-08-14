51afff707c1d494e93329b4c58746dedb711048c
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-08-14 23:45:10 +1000

    implemented union by rank

--- course_3/week_5/assignment_1/src/main/java/ConnectingPoints.java
+++ course_3/week_5/assignment_1/src/main/java/ConnectingPoints.java
@@ -6,53 +6,6 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class ConnectingPoints {
-  private static double minimumDistance(int[] x, int[] y) {
-    final int n = x.length;
-    Point[] pointArray = new Point[n]; 
-    Map<Point, DisjointSetNode> nodeMap = new HashMap<>();
-    
-    for (int i = 0; i < n; i++) {
-      pointArray[i] = new Point(x[i], y[i]);
-      DisjointSetNode node = new DisjointSetNode(pointArray[i]); 
-      nodeMap.put(pointArray[i], node);
-    }
-    
-    PriorityQueue<Edge> edgeQueue = new PriorityQueue<>();
-    
-    for (int i = 0; i < n - 1; i++) {
-      for (int j = i + 1; j < n; j++) {
-        Edge edge = new Edge(pointArray[i], pointArray[j]);
-        edgeQueue.offer(edge);
-      }
-    }
-    
-    double result = 0.0;
-    int edgeCount = 0;
-
-    while (!edgeQueue.isEmpty()) {
-      Edge edge = edgeQueue.poll();
-      Point point1 = edge.getPoint1();
-      Point point2 = edge.getPoint2();
-      DisjointSetNode node1 = nodeMap.get(point1);
-      DisjointSetNode node2 = nodeMap.get(point2);
-     
-      DisjointSetNode root1 = node1.findRoot();
-      DisjointSetNode root2 = node2.findRoot();
-      
-      if (root1 != root2) {
-        result += edge.getDistance();
-        root2.setParent(root1);
-        edgeCount++;
-        
-        if (edgeCount == n - 1) {
-          break;
-        }
-      }
-    }
-    
-    return result;
-  }
-
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
@@ -68,6 +21,69 @@ public class ConnectingPoints {
     scanner.close();
   }
 
+  private static double minimumDistance(int[] x, int[] y) {
+    final int n = x.length;
+    Point[] pointArray = new Point[n];
+    Map<Point, DisjointSetNode> nodeMap = new HashMap<>();
+
+    for (int i = 0; i < n; i++) {
+      pointArray[i] = new Point(x[i], y[i]);
+      DisjointSetNode node = new DisjointSetNode();
+      nodeMap.put(pointArray[i], node);
+    }
+
+    PriorityQueue<Edge> edgeQueue = new PriorityQueue<>();
+
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = i + 1; j < n; j++) {
+        Edge edge = new Edge(pointArray[i], pointArray[j]);
+        edgeQueue.offer(edge);
+      }
+    }
+
+    double result = 0.0;
+    int edgeCount = 0;
+
+    while (!edgeQueue.isEmpty()) {
+      Edge edge = edgeQueue.poll();
+      Point point1 = edge.getPoint1();
+      Point point2 = edge.getPoint2();
+      DisjointSetNode node1 = nodeMap.get(point1);
+      DisjointSetNode node2 = nodeMap.get(point2);
+
+      DisjointSetNode root1 = node1.findRoot();
+      DisjointSetNode root2 = node2.findRoot();
+
+      if (root1 != root2) {
+        result += edge.getDistance();
+        union(root1, root2);
+        edgeCount++;
+
+        if (edgeCount == n - 1) {
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  // union by rank
+  private static void union(DisjointSetNode root1, DisjointSetNode root2) {
+    int rank1 = root1.getRank();
+    int rank2 = root2.getRank();
+
+    if (rank1 > rank2) {
+      root2.setParent(root1);
+    } else {
+      root1.setParent(root2);
+
+      if (rank1 == rank2) {
+        root2.setRank(rank2 + 1);
+      }
+    }
+  }
+
   private static class Edge implements Comparable<Edge> {
     private final Point point1;
     private final Point point2;
@@ -78,7 +94,7 @@ public class ConnectingPoints {
       this.point2 = point2;
       this.distance = Point.computeDistance(point1, point2);
     }
-    
+
     public Point getPoint1() {
       return point1;
     }
@@ -90,7 +106,7 @@ public class ConnectingPoints {
     public double getDistance() {
       return distance;
     }
-    
+
     @Override
     public int compareTo(Edge otherEdge) {
       long d1 = Double.doubleToLongBits(distance);
@@ -106,7 +122,7 @@ public class ConnectingPoints {
 
       return 0;
     }
-    
+
     @Override
     public String toString() {
       return String.format("{%s, %s}, distance = %.4f", point1, point2, distance);
@@ -115,13 +131,12 @@ public class ConnectingPoints {
 
   @SuppressWarnings("unused")
   private static class DisjointSetNode {
-    private final Point point;
     private DisjointSetNode parent;
     private int rank;
-    
-    public DisjointSetNode(Point point) {
-      this.point = point;
+
+    public DisjointSetNode() {
       parent = this;
+      rank = 0;
     }
 
     public DisjointSetNode getParent() {
@@ -132,10 +147,6 @@ public class ConnectingPoints {
       this.parent = parent;
     }
 
-    public Point getPoint() {
-      return point;
-    }
-    
     public int getRank() {
       return rank;
     }
@@ -146,14 +157,14 @@ public class ConnectingPoints {
 
     public DisjointSetNode findRoot() {
       DisjointSetNode q = this;
-      
+
       List<DisjointSetNode> nodeList = new ArrayList<>();
 
       while (q != q.parent) {
-        nodeList.add(q); 
+        nodeList.add(q);
         q = q.parent;
       }
-      
+
       // path compression
       for (DisjointSetNode node : nodeList) {
         node.setParent(q);
@@ -162,35 +173,35 @@ public class ConnectingPoints {
       return q;
     }
   }
-  
+
   private static class Point {
     private final int x;
     private final int y;
-    
+
     public Point(int x, int y) {
       this.x = x;
       this.y = y;
     }
-    
+
     public static double computeDistance(Point p1, Point p2) {
       return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
     }
-    
+
     @Override
     public int hashCode() {
-      return 17*x + 19*y + 23;
+      return 17 * x + 19 * y + 23;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
       if (!(obj instanceof Point)) {
         return false;
       }
-      
+
       Point otherPoint = (Point) obj;
       return x == otherPoint.x && y == otherPoint.y;
     }
-    
+
     @Override
     public String toString() {
       return String.format("(%d, %d)", x, y);
