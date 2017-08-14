ad1f6264ca7b85e496afa5adca9859f691020d33
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-08-14 23:09:15 +1000

    added a preliminary solution that works on small inputs

--- course_3/week_5/assignment_1/src/main/java/ConnectingPoints.java
+++ course_3/week_5/assignment_1/src/main/java/ConnectingPoints.java
@@ -1,9 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class ConnectingPoints {
   private static double minimumDistance(int[] x, int[] y) {
-    double result = 0.;
-    // write your code here
+    final int n = x.length;
+    Point[] pointArray = new Point[n]; 
+    Map<Point, DisjointSetNode> nodeMap = new HashMap<>();
+    
+    for (int i = 0; i < n; i++) {
+      pointArray[i] = new Point(x[i], y[i]);
+      DisjointSetNode node = new DisjointSetNode(pointArray[i]); 
+      nodeMap.put(pointArray[i], node);
+    }
+    
+    PriorityQueue<Edge> edgeQueue = new PriorityQueue<>();
+    
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = i + 1; j < n; j++) {
+        Edge edge = new Edge(pointArray[i], pointArray[j]);
+        edgeQueue.offer(edge);
+      }
+    }
+    
+    double result = 0.0;
+
+    while (!edgeQueue.isEmpty()) {
+      Edge edge = edgeQueue.poll();
+      Point point1 = edge.getPoint1();
+      Point point2 = edge.getPoint2();
+      DisjointSetNode node1 = nodeMap.get(point1);
+      DisjointSetNode node2 = nodeMap.get(point2);
+     
+      DisjointSetNode root1 = node1.findRoot();
+      DisjointSetNode root2 = node2.findRoot();
+      
+      if (root1 != root2) {
+        result += edge.getDistance();
+        root2.setParent(root1);
+      }
+    }
+    
     return result;
   }
 
@@ -12,13 +52,143 @@ public class ConnectingPoints {
     int n = scanner.nextInt();
     int[] x = new int[n];
     int[] y = new int[n];
-    
+
     for (int i = 0; i < n; i++) {
       x[i] = scanner.nextInt();
       y[i] = scanner.nextInt();
     }
-    
-    System.out.println(minimumDistance(x, y));;
+
+    System.out.printf("%.9f%n", minimumDistance(x, y));;
     scanner.close();
+  }
+
+  private static class Edge implements Comparable<Edge> {
+    private final Point point1;
+    private final Point point2;
+    final double distance;
+
+    public Edge(Point point1, Point point2) {
+      this.point1 = point1;
+      this.point2 = point2;
+      this.distance = Point.computeDistance(point1, point2);
+    }
+    
+    public Point getPoint1() {
+      return point1;
+    }
+
+    public Point getPoint2() {
+      return point2;
+    }
+
+    public double getDistance() {
+      return distance;
+    }
+    
+    @Override
+    public int compareTo(Edge otherEdge) {
+      long d1 = Double.doubleToLongBits(distance);
+      long d2 = Double.doubleToLongBits(otherEdge.distance);
+
+      if (d1 < d2) {
+        return -1;
+      }
+
+      if (d1 > d2) {
+        return 1;
+      }
+
+      return 0;
+    }
+    
+    @Override
+    public String toString() {
+      return String.format("{%s, %s}, distance = %.4f", point1, point2, distance);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private static class DisjointSetNode {
+    private final Point point;
+    private DisjointSetNode parent;
+    private int rank;
+    
+    public DisjointSetNode(Point point) {
+      this.point = point;
+    }
+
+    public DisjointSetNode getParent() {
+      return parent;
+    }
+
+    public void setParent(DisjointSetNode parent) {
+      this.parent = parent;
+    }
+
+    public Point getPoint() {
+      return point;
+    }
+    
+    public int getRank() {
+      return rank;
+    }
+
+    public void setRank(int rank) {
+      this.rank = rank;
+    }
+
+    public DisjointSetNode findRoot() {
+      DisjointSetNode p = parent;
+      DisjointSetNode q = this;
+      
+      List<DisjointSetNode> nodeList = new ArrayList<>();
+
+      while (p != null) {
+        nodeList.add(q); 
+        q = p;
+        p = p.parent;
+      }
+      
+      // path compression
+      for (DisjointSetNode node : nodeList) {
+        node.setParent(q);
+      }
+
+      return q;
+    }
+  }
+  
+  private static class Point {
+    private final int x;
+    private final int y;
+    
+    public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+    
+    public static double computeDistance(Point p1, Point p2) {
+      return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+    
+    @Override
+    public int hashCode() {
+      return 17*x + 19*y + 23;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Point)) {
+        return false;
+      }
+      
+      Point otherPoint = (Point) obj;
+      return x == otherPoint.x && y == otherPoint.y;
+    }
+    
+    @Override
+    public String toString() {
+      return String.format("(%d, %d)", x, y);
+    }
   }
 }
