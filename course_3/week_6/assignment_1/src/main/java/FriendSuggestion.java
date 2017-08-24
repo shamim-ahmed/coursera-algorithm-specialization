76ee0fe2217822ceb19d9693b3b35405aa742a25
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-08-24 19:32:54 +1000

    added a tentative solution to assignment 1

--- course_3/week_6/assignment_1/src/main/java/FriendSuggestion.java
+++ course_3/week_6/assignment_1/src/main/java/FriendSuggestion.java
@@ -14,60 +14,123 @@ public class FriendSuggestion {
     ArrayList<Integer>[][] cost;
     // distance[0] and distance[1] correspond to distance estimates in the forward and backward
     // searches.
-    Long[][] distance;
+    long[][] distance;
     // Two priority queues, one for forward and one for backward search.
-    ArrayList<PriorityQueue<Entry>> queue;
+    ArrayList<PriorityQueue<Entry>> queueList;
     // visited[v] == true iff v was visited either by forward or backward search.
     boolean[] visited;
     // List of all the nodes which were visited either by forward or backward search.
     ArrayList<Integer> workset;
     final Long INFINITY = Long.MAX_VALUE / 4;
-
+    
+    Entry[] entries;
+    
     Impl(int n) {
       this.n = n;
       visited = new boolean[n];
       Arrays.fill(visited, false);
       workset = new ArrayList<Integer>();
-      distance = new Long[][] {new Long[n], new Long[n]};
+      distance = new long[][] {new long[n], new long[n]};
+      
       for (int i = 0; i < n; ++i) {
         distance[0][i] = distance[1][i] = INFINITY;
       }
-      queue = new ArrayList<PriorityQueue<Entry>>();
-      queue.add(new PriorityQueue<Entry>(n));
-      queue.add(new PriorityQueue<Entry>(n));
+      
+      entries = new Entry[n];
+      for (int i = 0; i < n; i++) {
+        entries[i] = new Entry(INFINITY, i);
+      }
+
+      queueList = new ArrayList<PriorityQueue<Entry>>();
+      queueList.add(new PriorityQueue<Entry>(n));
+      queueList.add(new PriorityQueue<Entry>(n));
     }
 
-    // Reinitialize the data structures before new query after the previous query
+    // Reinitialize the data structures before new query after the previous query 
     void clear() {
       for (int v : workset) {
         distance[0][v] = distance[1][v] = INFINITY;
         visited[v] = false;
       }
+      
+      for (int i = 0; i < n; i++) {
+        entries[i].cost = INFINITY;
+      }
+      
       workset.clear();
-      queue.get(0).clear();
-      queue.get(1).clear();
-    }
-
-    // Try to relax the distance from direction side to node v using value dist.
-    void visit(int side, int v, Long dist) {
-      // Implement this method yourself
+      queueList.get(0).clear();
+      queueList.get(1).clear();
     }
 
     // Returns the distance from s to t in the graph.
     Long query(int s, int t) {
       clear();
-      visit(0, s, 0L);
-      visit(1, t, 0L);
-      // Implement the rest of the algorithm yourself
+      
+      // initialize for forward search
+      distance[0][s] = 0L;
+      entries[s].cost = 0L;
+      PriorityQueue<Entry> forwardQueue = queueList.get(0);
+      forwardQueue.offer(entries[s]);
+      
+      // initialize for backward search
+      distance[1][t] = 0L;
+      entries[t].cost = 0L;
+      PriorityQueue<Entry> backwardQueue = queueList.get(1);
+      backwardQueue.offer(entries[t]);
+      
+      boolean done = false;
+      
+      while (!done && !forwardQueue.isEmpty() && !backwardQueue.isEmpty()) {
+        done = process(forwardQueue, 0);
+        
+        if (!done) {
+          done = process(backwardQueue, 1);
+        }
+      }  
+      
+      long result = INFINITY;
+      
+      for (int v : workset) {
+        if (result > distance[0][v] + distance[1][v]) {
+          result = distance[0][v] + distance[1][v];
+        }
+      }
 
-      return -1L;
+      return result == INFINITY ? -1 : result;
+    }
+    
+    boolean process(PriorityQueue<Entry> pQueue, int side) {
+      Entry entry = pQueue.poll();
+      int u = entry.node;
+
+      if (visited[u]) {
+        return true;
+      }
+      
+      visited[u] = true;
+      workset.add(u);
+
+      for (int i = 0, len = adj[side][u].size(); i < len; i++) {
+        int v = adj[side][u].get(i);
+        int edgeWeight = cost[side][u].get(i);
+
+        if (distance[side][v] > distance[side][u] + edgeWeight) {
+          distance[side][v] = distance[side][u] + edgeWeight;
+          Entry adjEntry = entries[v];
+          pQueue.remove(adjEntry);
+          adjEntry.cost = distance[side][v];
+          pQueue.offer(adjEntry);
+        }
+      }
+
+      return false;
     }
 
     class Entry implements Comparable<Entry> {
-      Long cost;
+      long cost;
       int node;
 
-      public Entry(Long cost, int node) {
+      public Entry(long cost, int node) {
         this.cost = cost;
         this.node = node;
       }
@@ -78,6 +141,7 @@ public class FriendSuggestion {
     }
   }
 
+  @SuppressWarnings("unchecked")
   public static void main(String args[]) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
@@ -85,9 +149,11 @@ public class FriendSuggestion {
     Impl bidij = new Impl(n);
     bidij.adj = (ArrayList<Integer>[][]) new ArrayList[2][];
     bidij.cost = (ArrayList<Integer>[][]) new ArrayList[2][];
+    
     for (int side = 0; side < 2; ++side) {
       bidij.adj[side] = (ArrayList<Integer>[]) new ArrayList[n];
       bidij.cost[side] = (ArrayList<Integer>[]) new ArrayList[n];
+      
       for (int i = 0; i < n; i++) {
         bidij.adj[side][i] = new ArrayList<Integer>();
         bidij.cost[side][i] = new ArrayList<Integer>();
@@ -95,10 +161,9 @@ public class FriendSuggestion {
     }
 
     for (int i = 0; i < m; i++) {
-      int x, y, c;
-      x = in.nextInt();
-      y = in.nextInt();
-      c = in.nextInt();
+      int x = in.nextInt();
+      int y = in.nextInt();
+      int c = in.nextInt();
       bidij.adj[0][x - 1].add(y - 1);
       bidij.cost[0][x - 1].add(c);
       bidij.adj[1][y - 1].add(x - 1);
@@ -113,5 +178,7 @@ public class FriendSuggestion {
       v = in.nextInt();
       System.out.println(bidij.query(u - 1, v - 1));
     }
+    
+    in.close();
   }
 }
